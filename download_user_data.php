e288a01aac183d58d0206540d2185e94e5d87154
René <renedominik.heidbrink@studium.fernuni-hagen.de>
2023-09-03 18:58:06 +0200

    Sachstand zur Besprechung welche Änderungen erforderlich sind

--- download_user_data.php
+++ download_user_data.php
@@ -7,31 +7,36 @@ $PAGE->set_context($context);
 require_login();
 
 // transfer of the columns to be checked
-$userid =  $_POST['userid'];
+$selectedcolumn =  $_POST['selectedcolumn'];
+$columnvalue =  $_POST['columnvalue'];
 
 // Create an empty string
-$sql = '';
+$sql1 = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'moodle'";
 
 // Get a list of all tables in the database
-$tables = $DB->get_records_sql("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'moodle'");
+$tableschema = $DB->get_records_sql($sql1);
 
 // For each table, add a separate SELECT query
-foreach ($tables as $key => $inside) {
-    foreach ($inside as $inner_key => $value1) {
+foreach ($tableschema as $key => $tablename) {
+    foreach ($tablename as $inner_key => $chosentable) {
 
         // Run the SQL query and get the results
-        $sql1 = "SELECT * FROM $value1";
-        $results = $DB->get_records_sql($sql1);
+        $sql2 = "SELECT * FROM mdl_adminpresets  where $selectedcolumn  = $columnvalue ";
+        $results = $DB->get_records_sql($sql2);
 
-        // save Table in File
-        $filename = $value1.".csv";
-        $file = fopen($filename, 'w');
+         // save Table in File
+         $filename = $chosentable.".csv";
+         $file = fopen($filename, 'w');
+ 
+        // Write HTML request to CSV
+        foreach ($results as $result) {
 
-        foreach ($results as $fields) {
-        if( is_object($fields) )
-        $fields = (array) $fields;
-        fputcsv($file, $fields, ";");
-        }   
+            foreach ($result as $column => $value) {
+                $myvalue = $column.";".$value;
+                $myvalue = (array) $myvalue;
+                fputcsv($file, $myvalue);
+            }
+        }
         fclose($file);
 
         // create Zip-File
@@ -42,12 +47,12 @@ foreach ($tables as $key => $inside) {
     }
 }
 
-   // donwload Zip-File
-        $zipname = "download.zip";
-        $application="application/zip";
-        header( "Content-Type: $application" ); // Specify the format here
-        header( "Content-Disposition: attachment; filename= download.zip" ); // Enter the file name here that is displayed as the default file name when downloading
-        //header("Content-Length: ". filesize($filename));
-        readfile($zipname); // Here the path + filename of the source image on the web server
+ // donwload Zip-File
+ $zipname = "download.zip";
+ $application="application/zip";
+ header( "Content-Type: $application" ); // Specify the format here
+ header( "Content-Disposition: attachment; filename= download.zip" ); // Enter the file name here that is displayed as the default file name when downloading
+ //header("Content-Length: ". filesize($filename));
+ readfile($zipname); // Here the path + filename of the source image on the web server
    
 ?>
